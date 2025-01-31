5782c531f04663aec07dc27fbcebbc49eaf6b6ce
MJSuttles <rolltiderolldad@gmail.com>
2025-01-30 18:33:05 -0600

    completed steps 1-5

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
+using System.Runtime.CompilerServices;
+using HoneyRaesAPI.Models;
+List<Customer> customers = new List<Customer>
+{
+    new Customer() { Id = 100, Name = "Bob Marley", Address = "345 Maple Street" },
+    new Customer() { Id = 101, Name = "Sean Connery", Address = "678 Ocean Drive" },
+    new Customer() { Id = 102, Name = "Genghis Khan", Address = "1234 Elmwood Avenue" }
+};
+
+List<Employee> employees = new List<Employee>
+{
+    new Employee() { Id = 200, Name = "Steve Perry", Speciality = "Singing" },
+    new Employee() { Id = 201, Name = "David Coverdale", Speciality = "Screaming" }
+};
+
+List<ServiceTicket> serviceTickets = new List<ServiceTicket>
+{
+    new ServiceTicket() { Id = 1, CustomerId = 100, EmployeeId = 200, Description = "Plugged-up toilet", Emergency = false, DateCompleted = new DateTime(2025, 01, 28) },
+    new ServiceTicket() { Id = 2, CustomerId = 101, EmployeeId = 201, Description = "Exposed wires", Emergency = true },
+    new ServiceTicket() { Id = 3, CustomerId = 102, Description = "Busted mailbox", Emergency = false },
+    new ServiceTicket() { Id = 4, CustomerId = 101, EmployeeId = 200, Description = "Non-functioning monitor", Emergency = false },
+    new ServiceTicket() { Id = 5, CustomerId = 100, Description = "Leaky faucet", Emergency = true }
+ };
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,24 +45,14 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/servicetickets", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    return serviceTickets;
+});
+
+app.MapGet("/servicetickets/{id}", (int id) =>
+{
+    return serviceTickets.FirstOrDefault(st => st.Id == id);
+});
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
