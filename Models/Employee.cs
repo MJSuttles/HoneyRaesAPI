48c2f2a3a6cdf813b48496fb4bb20b27d7309234
MJSuttles <rolltiderolldad@gmail.com>
2025-02-01 10:53:57 -0600

    setup all endpoints

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -5,4 +5,5 @@ public class Employee
   public int Id { get; set; }
   public string Name { get; set; }
   public string Speciality { get; set; }
+  public List<ServiceTicket> ServiceTickets { get; set; }
 }
